cdb83649ab9c7314556d7b733c27d3dc29348e3e
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-05 00:48:43 +0500

    Ex68 solved

---
+++ HomeWork_02_09_22/Ex68_recurs_function_Ankerman/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ex68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//  Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int FunctionAnkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAnkerman(m - 1, 1);
+        else if (m > 0 && n > 0) return FunctionAnkerman(m - 1, FunctionAnkerman(m, n - 1));
+            else return -1;
+}
+Console.Write("Введите число M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("и число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"A({M},{N}) = {FunctionAnkerman(M, N)}");
