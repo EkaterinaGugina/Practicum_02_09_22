e6a3029b9db4015778df0c596fdf6c93243148b2
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-04 14:01:02 +0500

    Ex63 solved

--- Ex63_for_num_rec1N/Program.cs
+++ Ex63_for_num_rec1N/Program.cs
@@ -2,14 +2,15 @@
 //N = 5. -> ""1, 2, 3, 4, 5""
 void PrintNum1N(int n)                  //заполнение и печать массива
 {
-    if (n == 1) Console.Write($"{n}");
+    if (n == 1) Console.Write($"{n} ");
     else 
     {
         PrintNum1N(n - 1);
-        Console.WriteLine($"{n} ");
+        Console.Write($"{n} ");
     }
 }
 
 Console.Write("Введите число N = ");
 int N = Convert.ToInt32(Console.ReadLine());
 PrintNum1N(N);
+
