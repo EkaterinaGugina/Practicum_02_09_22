5e7ab9720a2d2767b420fc0ecaf2312ae20211c3
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-04 23:16:04 +0500

    Ex65 solved

---
+++ Ex65_for_num_recMN/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Ex65: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+//M = 4; N = 8. -> ""4, 5, 6, 7, 8""
+
+void PrintNumMN(int m, int n)
+{                 
+    if (m <= n) 
+    {
+        PrintNumMN(m, n - 1);
+        Console.Write($"{n} ");
+    }
+}
+Console.Write("Введите число M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("и число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintNumMN(M, N);
+
