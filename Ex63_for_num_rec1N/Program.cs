bb1a48ec98fe76050161afe3041afd352136daf5
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-04 13:56:20 +0500

    Ex63 solved

---
+++ Ex63_for_num_rec1N/Program.cs
@@ -0,0 +1,15 @@
+﻿// Ex63: Задайте значение N. Напишите программу, которая выведет все натуральные числа от 1 до N.
+//N = 5. -> ""1, 2, 3, 4, 5""
+void PrintNum1N(int n)                  //заполнение и печать массива
+{
+    if (n == 1) Console.Write($"{n}");
+    else 
+    {
+        PrintNum1N(n - 1);
+        Console.WriteLine($"{n} ");
+    }
+}
+
+Console.Write("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintNum1N(N);
