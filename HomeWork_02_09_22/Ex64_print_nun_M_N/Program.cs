90972e88f8644f795bb6f21df1089432ad1759e5
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-04 23:43:53 +0500

    Ex64  solved

--- HomeWork_02_09_22/Ex64_print_nun_M_N/Program.cs
+++ HomeWork_02_09_22/Ex64_print_nun_M_N/Program.cs
@@ -2,22 +2,16 @@
 //которая выведет все натуральные числа в промежутке от M до N в обратном порядке.
 //M = 1; N = 5. -> ""5, 4, 3, 2, 1""
 //M = 4; N = 8. -> ""8, 7, 6, 5, 4""
-
-void PrintNumMN(int m, int n)
+void PrintNumMNrevers(int m, int n)
 {                 
     if (m <= n) 
     {
         Console.Write($"{n} ");
-        PrintNumMN(m, n - 1);
-    }
-    else 
-    {
-        Console.WriteLine();
+        PrintNumMNrevers(m, n - 1);
     }
 }
 Console.Write("Введите число M = ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("и число N = ");
 int N = Convert.ToInt32(Console.ReadLine());
-PrintNumMN(M, N);
-
+PrintNumMNrevers(M, N);
